caec4855431efa893aafada3c70f238d78c0054a
John Zeringue <zeringus@gmail.com>
2014-06-16 15:37:56 -0400

    FXToSVGConverter implements ToFileConverter
    FXToSVGConverter now is able to convert nodes directly into an SVG file.

--- src/main/java/org/cirdles/convertfx/tosvg/FXToSVGConverter.java
+++ src/main/java/org/cirdles/convertfx/tosvg/FXToSVGConverter.java
@@ -15,13 +15,24 @@
  */
 package org.cirdles.convertfx.tosvg;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.Node;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import org.cirdles.convertfx.FXConverter;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.cirdles.convertfx.Converter;
+import org.cirdles.convertfx.ToFileConverter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -29,14 +40,16 @@ import org.w3c.dom.Element;
  *
  * @author John Zeringue <john.joseph.zeringue@gmail.com>
  */
-public class FXToSVGConverter implements FXConverter<Document> {
+public class FXToSVGConverter implements Converter<Node, Document>, ToFileConverter<Node> {
 
     private DocumentBuilder documentBuilder;
+    private Transformer transformer;
 
     public FXToSVGConverter() {
         try {
             documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        } catch (ParserConfigurationException ex) {
+            transformer = TransformerFactory.newInstance().newTransformer();
+        } catch (ParserConfigurationException | TransformerConfigurationException ex) {
             Logger.getLogger(FXToSVGConverter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -55,6 +68,27 @@ public class FXToSVGConverter implements FXConverter<Document> {
                 new GenericNodeConverter(svgDocument).convert(node));
 
         return svgDocument;
+    }
+
+    @Override
+    public void convertToFile(Node target, File file) {
+        try {
+            transformer.setOutputProperty(
+                    OutputKeys.DOCTYPE_PUBLIC, "-//W3C//DTD SVG 1.1//EN");
+            transformer.setOutputProperty(
+                    OutputKeys.DOCTYPE_SYSTEM, "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd");
+            transformer.setOutputProperty(
+                    javax.xml.transform.OutputKeys.INDENT, "yes");
+
+            transformer.transform(new DOMSource(convert(target)), new StreamResult(file));
+        } catch (TransformerException ex) {
+            Logger.getLogger(FXToSVGConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void convertToFile(Node target, Path path) {
+        convertToFile(target, path.toFile());
     }
 
     @Override
